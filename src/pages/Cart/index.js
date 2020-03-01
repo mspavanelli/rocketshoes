2a8000d9293fd17222651c4dfa892426d467aeb6
Matheus Pavanelli <mspavanelli@gmail.com>
2020-03-01 20:54:03 -0300

    feat(cart): add styles

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,5 +1,66 @@
 import React from 'react'
+import { Container, ProductTable, Total } from './styles'
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from 'react-icons/md'
 
-const Cart = () => <div>Cart</div>
+const Cart = () => (
+  <Container>
+    <ProductTable>
+      <thead>
+        <tr>
+          <th></th>
+          <th>Produto</th>
+          <th>Quantidade</th>
+          <th>Subtotal</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>
+            <img
+              src="https://images-na.ssl-images-amazon.com/images/I/61utX8kBDlL._UL1100_.jpg"
+              alt=""
+            />
+          </td>
+          <td>
+            <strong>Tênis</strong>
+            <span>R$ 129,00</span>
+          </td>
+          <td>
+            <div>
+              <button type="button">
+                <MdRemoveCircleOutline size="20" color="#7159c1" />
+              </button>
+              <input type="number" readOnly />
+              <button type="button">
+                <MdAddCircleOutline size="20" color="#7159c1" />
+              </button>
+            </div>
+          </td>
+          <td>
+            <strong>R$ 34,00</strong>
+          </td>
+          <td>
+            <button type="button">
+              <MdDelete size="20" color="#7159c1" />
+            </button>
+          </td>
+        </tr>
+      </tbody>
+    </ProductTable>
+
+    <footer>
+      <button type="button">Finalizar pedido</button>
+      <Total>
+        <span>Total</span>
+        <strong>R$ 100,00</strong>
+      </Total>
+    </footer>
+  </Container>
+)
 
 export default Cart
---
+++ src/pages/Cart/styles.js
@@ -0,0 +1,96 @@
+import styled from 'styled-components'
+import { darken } from 'polished'
+
+export const Container = styled.div`
+  background: #fff;
+  border-radius: 4px;
+  padding: 30px;
+
+  footer {
+    align-items: center;
+    display: flex;
+    justify-content: space-between;
+    margin-top: 30px;
+
+    button {
+      background-color: #7159c1;
+      border: 0;
+      border-radius: 4px;
+      color: #fff;
+      font-weight: 600;
+      padding: 12px 20px;
+      text-transform: uppercase;
+      transition: opacity 0.2s;
+
+      &:hover {
+        background-color: ${darken(0.03, '#7159c1')};
+      }
+    }
+  }
+`
+
+export const ProductTable = styled.table`
+  width: 100%;
+
+  thead th {
+    color: #999;
+    text-align: left;
+    padding: 12px;
+  }
+
+  tbody td {
+    border-bottom: 1px solid #eee;
+    padding: 12px;
+  }
+
+  img {
+    width: 100px;
+  }
+
+  strong {
+    color: #333;
+    display: block;
+  }
+
+  span {
+    display: block;
+    font-size: 18px;
+    font-weight: 600;
+    margin-top: 5px;
+  }
+
+  div {
+    align-items: center;
+    display: flex;
+
+    input {
+      border: 1px solid #ddd;
+      border-radius: 4px;
+      color: #666;
+      padding: 6px;
+      width: 50px;
+    }
+  }
+
+  button {
+    background: none;
+    border: 0;
+    padding: 6px;
+    outline: 0;
+  }
+`
+
+export const Total = styled.div`
+  display: flex;
+  align-items: baseline;
+
+  span {
+    color: #999;
+    font-weight: bold;
+  }
+
+  strong {
+    font-size: 28px;
+    margin-left: 5px;
+  }
+`
